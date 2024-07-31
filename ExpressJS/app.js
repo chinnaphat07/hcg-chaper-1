16d554f4e13c2afc2ce3a758a99afa27c2e8018f
chinnaphat07 <140487703+chinnaphat07@users.noreply.github.com>
2024-07-31 08:58:38 +0000

    Week7

---
+++ ExpressJS/app.js
@@ -0,0 +1,12 @@
+let http = require('http');
+let url = require('url');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+/*Use the url module to turn the querystring into an object:*/
+var q = url.parse(req.url, true).query;
+/*Return the year and month from the query object:*/
+
+var txt = "Name: "+ q.name + "<br/>" +"Login Date:"+ q.month +" "+ q.date + "  " + q.year;
+/*var txt = q.year+ " " + q.month;*/
+res.end(txt);
+}).listen(9999);
